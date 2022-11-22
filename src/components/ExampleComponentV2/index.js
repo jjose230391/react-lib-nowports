53295c8f73fe8d84a39ce35d8139697e0412fa85
jose.perez <jose.perez@nowports.com>
2022-11-22 12:20:45 -0300

    Add RotInput from rotterdamV2

--- src/components/ExampleComponentV2/index.js
+++ src/components/ExampleComponentV2/index.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
+import { RotInput } from "rotterdam-v2";
 import getChats from "../../data/chat";
-import {MainLayoutContext} from "../../context/ChatProvider";
+import { MainLayoutContext } from "../../context/ChatProvider";
 
 class ExampleV2 extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { text: "" };
   }
 
   componentDidMount() {
@@ -17,21 +18,23 @@ class ExampleV2 extends Component {
     // this.fetchChats();
 
     //
-    socket.on('NewMessage', (data) => {
-        onSubmit(data)
+    socket.on("NewMessage", (data) => {
+      onSubmit(data);
     });
   }
 
   verifyConnectionSocket = () => {
-    const { config: { token } } = this.props
+    const {
+      config: { token },
+    } = this.props;
     const { socket, onRegisterUserIOToken } = this.context;
     socket.on("connect", () => {
-        onRegisterUserIOToken(token)
-        if (socket.connected) {
-            console.log('socket conectado')
-        }else{
-            console.log('socket no conectado')
-        }
+      onRegisterUserIOToken(token);
+      if (socket.connected) {
+        console.log("socket conectado");
+      } else {
+        console.log("socket no conectado");
+      }
     });
   };
 
@@ -48,15 +51,24 @@ class ExampleV2 extends Component {
 
   handleText = (e) => {
     const { onEmitNewMessage } = this.context;
+    this.setState({ text: e.target.value });
     onEmitNewMessage({ message: e.target.value });
   };
 
   render() {
     const { children } = this.props;
+    const { text } = this.state;
     return (
       <>
         <h2>{children}</h2>
-        <input type="text" onChange={(e) => this.handleText(e)} />
+        <RotInput
+          placeholder="Hola mundo"
+          label="Input"
+          value={text}
+          onChange={(e) => {
+            this.handleText(e);
+          }}
+        />
       </>
     );
   }
@@ -67,13 +79,13 @@ class ExampleV2 extends Component {
 // }
 ExampleV2.defaultProps = {
   children: null,
-  config: {token: ''},
-  onSubmit: () => {}
+  config: { token: "" },
+  onSubmit: () => {},
 };
 ExampleV2.propTypes = {
   children: PropTypes.node,
-  config: PropTypes.shape({token: PropTypes.string}),
-  onSubmit: PropTypes.func
+  config: PropTypes.shape({ token: PropTypes.string }),
+  onSubmit: PropTypes.func,
 };
 
 ExampleV2.contextType = MainLayoutContext;
